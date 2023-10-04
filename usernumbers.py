704966f6628278e6eb20cdf608ac5190a81b49e6
Cal Lewis <clisatree@gmail.com>
2023-10-04 12:14:40 -0400

    usernumbers.py

---
+++ usernumbers.py
@@ -0,0 +1,12 @@
+def main() :
+    print("This program adds two numbers.")
+
+    number1 = input("25")
+    number2 = input("30")
+
+    total =int(number1) + int(number2)
+
+    print ("The total is " + str(total) + ".")
+
+if __name__ == "__main__":
+     main()
