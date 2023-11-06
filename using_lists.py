506c38b28c62ba7e8acfc8f61ea307ce86d0e03c
Cal Lewis <clisatree@gmail.com>
2023-11-06 11:23:37 -0500

    (1162023)usinglists

---
+++ using_lists.py
@@ -0,0 +1,15 @@
+def main():
+     nums = [3, 41, 12, 9, 74, 15]
+     print(len(nums))
+     print(max(nums))
+     print(min(nums))
+     print(sum(nums))
+     x = sum(nums)
+     y = len(nums)
+     avg = x/y
+     print(avg)
+
+
+
+if __name__ == '__main__':
+     main()
