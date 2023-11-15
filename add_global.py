61f631089983bfba0b28b88b80bc1514abd542f8
Cal Lewis <clisatree@gmail.com>
2023-11-15 11:41:39 -0500

    20231511

---
+++ add_global.py
@@ -0,0 +1,11 @@
+Var = 42
+
+def add(num1):
+     return(num1 + Var )
+
+def main():
+     number = add(10)
+     print("Sum:", number, "is a whole number")
+
+if __name__ == '__main__':
+     main()
